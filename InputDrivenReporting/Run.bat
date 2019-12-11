5264745673e8ac86799838b36687fe4e3b89a189
Shivam Maralay <shivam201@gmail.com>
2019-12-11 18:51:43 +0530

    Update Run.bat

--- InputDrivenReporting/Run.bat
+++ InputDrivenReporting/Run.bat
@@ -3,15 +3,14 @@ rem "This jar will take argument as EXCEL or XML or CSV"
 rem "Run this option for running jar"
 rem java -jar Reporting.jar EXCEL
 
-rem "If java -jar gives error run this -> java -cp Jarname Package.ClassnameEntrypoint argument1"
 rem java -cp Reporting.jar Reporting.Reporting CSV
 rem java -cp Reporting.jar Reporting.Reporting XML
 rem java -cp Reporting.jar Reporting.Reporting EXCEL
 
-args[0] - Current directory or the jar file.
-args[1] - CSV or XML or EXCEL
-args[2] - Page Title
-args[3] - Top Title
+rem args[0] - Current directory or the jar file.
+rem args[1] - CSV or XML or EXCEL
+rem args[2] - Page Title
+rem args[3] - Top Title
 java -cp .\Reporting\Reporting_POC\Reporting.jar Reporting.Reporting "F:\Java Project Repository\Reporting\Reporting_POC" CSV CAFeReporting ShivamReporting
 
 pause
